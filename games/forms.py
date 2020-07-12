89fd98209903a5035b0f36adf7411bc94edeeae2
andrewgark <andrewgarkavyy@gmail.com>
2020-07-13 00:40:42 +0300

    frontend improvement

--- games/forms.py
+++ games/forms.py
@@ -3,6 +3,10 @@ from games.models import Team
 
 
 class CreateTeamForm(ModelForm):
+    def __init__(self, *args, **kwargs): 
+        super(ModelForm, self).__init__(*args, **kwargs)
+        self.fields['name'].label = 'Название'
+
     class Meta:
         model = Team
         fields = ['name']
@@ -11,10 +15,13 @@ class CreateTeamForm(ModelForm):
 class JoinTeamForm(Form):
     def __init__(self, *args, **kwargs):
         super(JoinTeamForm, self).__init__(*args, **kwargs)
-        self.fields['name'].choices = [(x.name, x.name) for x in sorted(Team.objects.all(), key=lambda t: t.name)]
+        self.fields['name'].choices = [('', 'Выберите команду')]
+        for x in sorted(Team.objects.all(), key=lambda t: t.name):
+            self.fields['name'].choices.append((x.name, x.name))
 
     name = ChoiceField(
         choices=(),
         initial='Выберите команду',
-        required=True
+        required=True,
+        label='Название'
     )
