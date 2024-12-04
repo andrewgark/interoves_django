5f7289c43989a7835a4c3764d684c225206b055f
Andrei Garkavyi <andrewgarkavyy@gmail.com>
2024-12-04 23:41:04 +0400

    use asgi instead of wsgi in aws

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: daphne -b 0.0.0.0 -p 8000 interoves_django.asgi:application
--- interoves_django/asgi.py
+++ interoves_django/asgi.py
@@ -1,4 +1,7 @@
 import os
+from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interoves_django.settings')
+django_asgi_app = get_asgi_application()
 
 import django
 # from channels.http import AsgiHandler
@@ -9,11 +12,10 @@ from django.core.asgi import get_asgi_application
 from games.views.track import TrackGame
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interoves_django.settings')
 django.setup()
 
 application = ProtocolTypeRouter({
-  "http":  get_asgi_application(),
+  "http":  django_asgi_app,
   "websocket": AuthMiddlewareStack(
         URLRouter([
             re_path(r'^games/(?P<game_id>[a-zA-Z0-9_]+)/track/?$', TrackGame.as_asgi()),
